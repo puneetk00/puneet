96625f77096c26420e5c0891c3e53acad98f3df7
puneetk00 <puneetk00@gmail.com>
2017-02-09 13:57:26 +0530

    Update list.php

--- list.php
+++ list.php
@@ -1,1 +1,36 @@
-<?php echo $this->getChildHtml('filter.catalog'); ?>
+jQuery(function($) {
+  function fixDiv() {
+    var $cache = $('#catalog-filters');
+    var $cache_1 = $('.toolbar:first').find('.sort-by');
+    if ($(window).scrollTop() > 100){
+      $cache.css({
+        'position': 'fixed',
+        'width': '100%',
+        'left': '0',
+        'z-index': '99999999',
+        'top': '32px'
+      });
+	  $cache_1.css({
+        'position': 'fixed',
+        'background': '#EFEFEF',
+        'right': '0',
+        'z-index': '99999999',
+        'padding': '27px  19px',
+        'top': '5px'
+      });
+    }else{
+      $cache.css({
+        'position': '',
+        'top': 'auto'
+      });
+	  $cache_1.css({
+        'position': '',
+		'background': '',
+		'padding': '0',
+        'top': 'auto'
+      });
+	}
+  }
+  $(window).scroll(fixDiv);
+  fixDiv();
+});
